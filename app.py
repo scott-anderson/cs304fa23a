3b70d545c6e8d4c54622c42188074bb7f412d6c5
Scott Personal <scott@scott-anderson.org>
2023-11-01 08:35:37 -0400

    modified database

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ if __name__ == '__main__':
     else:
         port = os.getuid()
     # set this local variable to 'wmdb' or your personal or team db
-    db_to_use = 'put_database_name_here_db' 
+    db_to_use = 'team_db'
     print('will connect to {}'.format(db_to_use))
     dbi.conf(db_to_use)
     app.debug = True
